8f5c9a94c462f9ae7ee6918f161c2cc3d935ccc3
zuiwuchang <zuiwuchang@gmail.com>
2024-02-04 10:27:06 +0800

    deps

---
+++ deps/deps.ts
@@ -0,0 +1,2 @@
+export * from "https://deno.land/std@0.214.0/assert/assert_equals.ts"
+export * from "https://deno.land/x/flags@0.0.3/mod.ts"
---
+++ deps/std/assert/assert_equals.ts
@@ -0,0 +1,1 @@
+export * from "https://deno.land/std@0.214.0/assert/assert_equals.ts"
